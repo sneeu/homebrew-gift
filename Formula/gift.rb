e8dcb814b3c9665391af47177d47fa5f28880f89
John Sutherland <john@sneeu.com>
2026-04-10 16:00:35 +0100

    chore: release v0.1.2

--- Formula/gift.rb
+++ Formula/gift.rb
@@ -10,20 +10,20 @@
 class Gift < Formula
   desc "Terminal UI for managing a GIF library stored in S3"
   homepage "https://github.com/sneeu/gift"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/sneeu/gift/releases/download/v#{version}/gift-macos-aarch64.tar.gz"
-      sha256 "eb43d5e2d6c22b006a42da341da635d1dae7289f1f9b009bb9fc8b72c24d453b"
+      sha256 "938f292d98a87d4b29ff802c99d974fb1885b21c369dd51c217f91a2c877e657"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/sneeu/gift/releases/download/v#{version}/gift-linux-x86_64.tar.gz"
-      sha256 "sha256:3b5fe0965a09feff925d2d67e165b6158baa1bc13b99d1cf08bb91158473bc19"
+      sha256 "30c453f3107ee8c0a228a380015e61af94d58c204911f78f9d9fb661afd9b66b"
     end
   end
 
